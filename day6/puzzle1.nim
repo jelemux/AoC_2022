ee29738e3f63b3a281eb1c748d392b549a2525c8
Jeremias Weber <jeremias.weber@cloudogu.com>
2022-12-06 12:16:01 +0100

    Day 6 Puzzle 1

---
+++ day6/puzzle1.nim
@@ -0,0 +1,21 @@
+import std/strutils
+import std/sequtils
+
+try:
+  let input = readFile("day6/input.txt")
+
+  var charsBeforeStart = 0
+  for i in 0 ..< input.len-4:
+    let sectionToObserve = input[i ..< i+4]
+    if sectionToObserve.deduplicate().len == 4:
+      echo sectionToObserve
+      echo sectionToObserve.deduplicate()
+      charsBeforeStart = i+4
+      break
+
+  echo "Chars before start-of-packet: ", charsBeforeStart
+except Exception:
+  let
+    e = getCurrentException()
+    msg = getCurrentExceptionMsg()
+  echo "Got exception ", repr(e), " with message ", msg
